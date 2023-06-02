be293025c5613f3b289221d4032e42edc7698a70
raghavBakshee <raghav_bakshee@yahoo.in>
2023-06-02 22:38:08 +0530

    cypress handson

--- cypress/e2e/firste2espec.cy.js
+++ cypress/e2e/firste2espec.cy.js
@@ -10,10 +10,10 @@ describe('Cypress Example', ()=>{
     cy.contains('type').click()
     cy.url().should('include','/commands/actions')
 
-    cy.get('.action-email').type('fake@email.com')
+    cy.get('.action-email').type('fake@email.com').blur()
 
     //  Verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com')
+    //cy.get('.action-email').should('have.value', 'fake@email.com')
     
   })
 })
